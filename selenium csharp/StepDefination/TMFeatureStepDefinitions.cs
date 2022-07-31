cd0a71bda5bc2cde19e36b749a11e3aa8f996ffd
KAVITHA CHIMATA <chimata.kavitha@gmail.com>
2022-08-01 02:40:21 +1200

    API tests added

--- selenium csharp/StepDefination/TMFeatureStepDefinitions.cs
+++ selenium csharp/StepDefination/TMFeatureStepDefinitions.cs
@@ -1,33 +1,41 @@
-using System;
 using TechTalk.SpecFlow;
+using selenium_csharp.utilities;
+using selenium_csharp.pages;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
-namespace selenium_csharp.StepDefination
+namespace selenium_csharp;
+
+[Binding]
+public class TMFeatureStepDefinitions: Commondriver
 {
-    [Binding]
-    public class TMFeatureStepDefinitions
+    [Given(@"I logged into TrnUp portal successfully")]
+    public void GivenILoggedIntoTrnUpPortalSuccessfully()
     {
-        [Given(@"I logged into TrnUp portal successfully")]
-        public void GivenILoggedIntoTrnUpPortalSuccessfully()
-        {
-            
-        }
 
-        [When(@"I navigate to Time and Meterial page")]
-        public void WhenINavigateToTimeAndMeterialPage()
-        {
-            
-        }
+    driver = new ChromeDriver();
 
-        [When(@"I create new meterial record")]
-        public void WhenICreateNewMeterialRecord()
-        {
-            
-        }
+    loginpage loginPageObj = new loginpage();
+    loginPageObj.LoginActions(driver);
+}
 
-        [Then(@"The record should be created sucessfully")]
-        public void ThenTheRecordShouldBeCreatedSucessfully()
-        {
-            
-        }
+    [When(@"I navigate to Time and Meterial page")]
+    public void WhenINavigateToTimeAndMeterialPage()
+    {
+        homepage homePageObj = new homepage();
+        homePageObj.GoToTMPage(driver);
+    }
+
+    [When(@"I create new meterial record")]
+    public void WhenICreateNewMeterialRecord()
+    {
+        TM_Page tmPageObj = new TM_Page();
+        tmPageObj.EditTM(driver);
+    }
+
+    [Then(@"The record should be created sucessfully")]
+    public void ThenTheRecordShouldBeCreatedSucessfully()
+    {
+        
     }
 }
